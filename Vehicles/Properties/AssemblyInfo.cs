a3c15c17d5bdd52acd58f7180bf6e5ac25aa3535
tronghieu180 <htrhieu2003@gmail.com>
2024-03-11 18:06:17 +0700

    Su dung LINQ

--- Vehicles/Properties/AssemblyInfo.cs
+++ Vehicles/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Company")]
+[assembly: AssemblyTitle("Vehicles")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Company")]
+[assembly: AssemblyProduct("Vehicles")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0b2bcc5c-bf1b-4423-ae37-0855984b7582")]
+[assembly: Guid("21f8ffe9-66f5-4788-8127-115d0aeb1f31")]
 
 // Version information for an assembly consists of the following four values:
 //
