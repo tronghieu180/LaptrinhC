88f425d59d25667c7591c09203d00d709ec3f0cd
tronghieu180 <htrhieu2003@gmail.com>
2024-03-23 11:35:24 +0700

    BT LINQ JOIN

---
+++ Company/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company
+{
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+        public DateTime Birthday  { get; set; } 
+        public static List<Employee> GetEmployees()
+        {
+            return new List<Employee>() { 
+                new Employee{ID = 1, Name = "A", Age = 30, Salary = 100000, Birthday = new DateTime(1997, 10, 20)},
+                new Employee{ID = 2, Name = "B", Age = 31, Salary = 200000, Birthday = new DateTime(1996, 10, 20)},
+                new Employee{ID = 3, Name = "C", Age = 32, Salary = 300000, Birthday = new DateTime(1995, 10, 20)},
+                new Employee{ID = 4, Name = "D", Age = 33, Salary = 400000, Birthday = new DateTime(1994, 10, 20)},
+                new Employee{ID = 5, Name = "E", Age = 34, Salary = 500000, Birthday = new DateTime(1993, 10, 20)},
+            };
+        }
+
+    }
+}
--- Company/Properties/AssemblyInfo.cs
+++ Company/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BTLINQ2")]
+[assembly: AssemblyTitle("Company")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BTLINQ2")]
+[assembly: AssemblyProduct("Company")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("77e10012-8129-4bc9-9c5f-7330fe8e36fe")]
+[assembly: Guid("0b2bcc5c-bf1b-4423-ae37-0855984b7582")]
 
 // Version information for an assembly consists of the following four values:
 //
